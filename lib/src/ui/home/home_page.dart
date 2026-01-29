9b0ee2db62bc6af635bcc45e3318f4277e13340e
junayedahamed <junayedahamed660@gmail.com>
2026-01-30 01:58:51 +0600

    Refactor HomePage layout to improve message display and input area

--- lib/src/ui/home/home_page.dart
+++ lib/src/ui/home/home_page.dart
@@ -80,135 +80,137 @@ class HomePage extends StatelessWidget {
             ),
             // Spacer(),
             Align(
-              alignment: AlignmentDirectional.bottomCenter,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
+              alignment: AlignmentDirectional.center,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text('Message $index'));
+                  },
                 ),
-                height: 150,
-                child: Column(
-                  children: [
-                    SizedBox(height: 15),
-                    Row(
+              ),
+            ),
+
+            Align(
+              alignment: AlignmentDirectional.bottomStart,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 4, 2, 2),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    height: 150,
+                    child: Stack(
+                      alignment: AlignmentDirectional.topCenter,
                       children: [
-                        Expanded(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Type a message',
-                              hintStyle: TextStyle(color: Colors.white54),
-                              contentPadding: EdgeInsets.all(10),
-                            ),
-                          ),
+                        ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Text('data');
+                          },
                         ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.send_outlined, color: Colors.white),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(height: 15),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Type a message',
+                                      hintStyle: TextStyle(
+                                        color: Colors.white54,
+                                      ),
+                                      contentPadding: EdgeInsets.all(10),
+                                    ),
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.send_outlined,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 15),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.add, color: Colors.white),
+                                  ),
+
+                                  SizedBox(
+                                    // height: 80,
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.75,
+                                    child: SingleChildScrollView(
+                                      child: Wrap(
+                                        spacing: 4.0,
+                                        runAlignment: WrapAlignment.start,
+                                        runSpacing: 4.0,
+                                        alignment: WrapAlignment.start,
+                                        crossAxisAlignment:
+                                            WrapCrossAlignment.center,
+                                        children: topic.map((tag) {
+                                          return Chip(
+                                            backgroundColor: Colors.grey[800],
+                                            label: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Icon(
+                                                  Icons.tag,
+                                                  color: Colors.white,
+                                                  size: 16,
+                                                ),
+                                                SizedBox(width: 5),
+                                                Text(
+                                                  tag,
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 12,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.voice_chat,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // SizedBox(height: 35),
+                          ],
                         ),
                       ],
                     ),
-                    SizedBox(height: 15),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.add, color: Colors.white),
-                          ),
-
-                          // Expanded(
-                          //   child: ListView.separated(
-                          //     scrollDirection: Axis.horizontal,
-                          //     // shrinkWrap: true,
-                          //     separatorBuilder: (context, index) {
-                          //       return SizedBox(width: 4);
-                          //     },
-                          //     itemCount: topic.length,
-                          //     itemBuilder: (context, index) {
-                          //       return Container(
-                          //         margin: EdgeInsets.only(top: 15, bottom: 15),
-                          //         padding: EdgeInsets.symmetric(
-                          //           horizontal: 12,
-                          //           vertical: 5,
-                          //         ),
-                          //         decoration: BoxDecoration(
-                          //           color: Colors.grey[800],
-                          //           borderRadius: BorderRadius.circular(20),
-                          //           border: Border.all(color: Colors.white54),
-                          //         ),
-                          //         child: Center(
-                          //           child: Row(
-                          //             children: [
-                          //               Icon(
-                          //                 Icons.tag,
-                          //                 color: Colors.white,
-                          //                 size: 16,
-                          //               ),
-                          //               SizedBox(width: 5),
-                          //               Text(
-                          //                 topic[index],
-                          //                 style: TextStyle(color: Colors.white),
-                          //               ),
-                          //             ],
-                          //           ),
-                          //         ),
-                          //       );
-                          //     },
-                          //   ),
-                          // ),
-                          SizedBox(
-                            // height: 80,
-                            width: MediaQuery.of(context).size.width * 0.75,
-                            child: SingleChildScrollView(
-                              child: Wrap(
-                                spacing: 4.0,
-                                runAlignment: WrapAlignment.start,
-                                runSpacing: 4.0,
-                                alignment: WrapAlignment.start,
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                children: topic.map((tag) {
-                                  return Chip(
-                                    backgroundColor: Colors.grey[800],
-                                    label: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Icon(
-                                          Icons.tag,
-                                          color: Colors.white,
-                                          size: 16,
-                                        ),
-                                        SizedBox(width: 5),
-                                        Text(
-                                          tag,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 12,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.voice_chat, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // SizedBox(height: 35),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
